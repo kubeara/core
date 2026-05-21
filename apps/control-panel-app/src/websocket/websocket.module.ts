4919b53956a7d44e65d0aa07204b564bead7adcc
Nitin <nitin.fst.1015@gmail.com>
2026-05-21 19:19:08 +0530

    fix the deployment module issue

--- apps/control-panel-app/src/websocket/websocket.module.ts
+++ apps/control-panel-app/src/websocket/websocket.module.ts
@@ -1,6 +1,6 @@
 import { Module, forwardRef } from "@nestjs/common";
 import { DeploymentGateway } from "./websocket.gateway";
-import { DeploymentsModule } from "../modules/deployments/deployments.module";
+import { DeploymentsModule } from "@control-panel/modules/deployments/deployments.module";
 
 @Module({
   imports: [forwardRef(() => DeploymentsModule)],
