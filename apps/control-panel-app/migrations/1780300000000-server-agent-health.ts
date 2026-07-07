8b83929379a0a39b33c3493868c1540a4cc8d556
dhaval-p-iqud <dhaval@iqudtek.com>
2026-07-07 16:12:31 +0530

    fix: migration for columns

--- apps/control-panel-app/migrations/1780300000000-server-agent-health.ts
+++ apps/control-panel-app/migrations/1780300000000-server-agent-health.ts
@@ -2,59 +2,44 @@ import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 
 export class ServerAgentHealth1780300000000 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.addColumn(
-      "servers",
+    await queryRunner.addColumns("servers", [
       new TableColumn({
         name: "isServerUp",
         type: "boolean",
         default: false,
         isNullable: false,
       }),
-    );
-
-    await queryRunner.addColumn(
-      "servers",
       new TableColumn({
         name: "lastAgentCheckedAt",
         type: "bigint",
         isNullable: true,
       }),
-    );
-
-    await queryRunner.addColumn(
-      "servers",
       new TableColumn({
         name: "retryCount",
         type: "integer",
         default: 0,
         isNullable: false,
       }),
-    );
-
-    await queryRunner.addColumn(
-      "servers",
       new TableColumn({
         name: "serverError",
         type: "jsonb",
         isNullable: true,
       }),
-    );
-
-    await queryRunner.addColumn(
-      "servers",
       new TableColumn({
         name: "agentError",
         type: "jsonb",
         isNullable: true,
       }),
-    );
+    ]);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropColumn("servers", "agentError");
-    await queryRunner.dropColumn("servers", "serverError");
-    await queryRunner.dropColumn("servers", "retryCount");
-    await queryRunner.dropColumn("servers", "lastAgentCheckedAt");
-    await queryRunner.dropColumn("servers", "isServerUp");
+    await queryRunner.dropColumns("servers", [
+      "agentError",
+      "serverError",
+      "retryCount",
+      "lastAgentCheckedAt",
+      "isServerUp",
+    ]);
   }
 }
